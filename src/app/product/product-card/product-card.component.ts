3b4f8b2917765bd1f51dcab9c4afb82fa00254d0
kirsenkodmitry <kirsenkodmitry@gmail.com>
2025-09-26 13:45:54 +0300

    added progress bar to product card

--- src/app/product/product-card/product-card.component.ts
+++ src/app/product/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
@@ -12,4 +12,23 @@ export class ProductCardComponent {
   @Input() id = '';
   @Input() name = '';
   @Input() price = 0;
+  @Input() requiredReserv = 0;
+  @Input() actualReserv = 0;
+
+  radius = 40;
+  circumference = 2 * Math.PI * this.radius;
+
+  // мы используем только половину окружности
+  halfCircumference = this.circumference / 2;
+
+  get progressPercent(): number {
+    if (this.requiredReserv === 0) return 0;
+    return Math.min(100, (this.actualReserv / this.requiredReserv) * 100);
+  }
+
+  get dashArray(): string {
+    const filled = (this.progressPercent / 100) * this.halfCircumference;
+    const empty = this.circumference;
+    return `${filled} ${empty}`;
+  }
 }
